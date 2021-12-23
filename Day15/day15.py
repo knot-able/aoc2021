cc09e8625fdf079f88fa54b82f583c905e86f201
knotable <sjarnold1@gmail.com>
2021-12-23 19:35:22 +0000

    Tidied into functions

--- Day15/day15.py
+++ Day15/day15.py
@@ -21,43 +21,31 @@ def adjacent(coord, m):
     
     return adj_coords
 
-def dijkstra():
-    pass
-
-
-if __name__ == "__main__":
-
-    INPUT = 'Day15/input15.txt'
-
-    with open(INPUT) as f:
-        chiton_map = []
-        for row in f.read().split('\n'):
-            chiton_map.append([int(c) for c in row])
-
+def lowest_risk_path(m):
+    """Performs dijkstra's algorithm on a map, m
+    consisting of a list of lists whose items are weights.
+    Returns lowest risk of bottom right position."""
+    
     # initialise values - dist, Q, S
-    dist = deepcopy(chiton_map)
+    dist = deepcopy(m)
     Q = []
     S = set()
 
-    for r in range(len(chiton_map)):
-        for c in range(len(chiton_map[0])):
+    for r in range(len(m)):
+        for c in range(len(m[0])):
             dist[r][c] = inf
             Q.append((r,c))
     dist[0][0] = 0
 
-# TODO - DON'T NEED TO RESORT THE WHOLE THING, JUST NEED TO REMOVE UPDATED ITEMS FROM LIST AND INSERT IN CORRECT PLACE
-# Go through list (already sorted) until you find the first item bigger than your item, then insert
-    update = False
-    
     # start dijkstra's algorithm
     while Q:
         current_position = Q.pop(0)
         if current_position not in S:
             S.add(current_position)
             current_weight = dist[current_position[0]][current_position[1]]
-            for a in adjacent(current_position, chiton_map):
+            for a in adjacent(current_position, m):
                 if current_weight + chiton_map[a[0]][a[1]] < dist[a[0]][a[1]]:
-                    dist[a[0]][a[1]] = current_weight + chiton_map[a[0]][a[1]]
+                    dist[a[0]][a[1]] = current_weight + m[a[0]][a[1]]
                     # insert a in correct place in list
                     if len(Q) > 1:
                         Q.remove(a)
@@ -71,10 +59,20 @@ if __name__ == "__main__":
                             if ind == len(Q):
                                 Q.append(a)
                                 a = False
-
-
     lowest_risk = dist[-1][-1]
-    print(f'Ans 1 : {lowest_risk}')
+    return lowest_risk
+
+if __name__ == "__main__":
+
+    INPUT = 'Day15/input15.txt'
+
+    with open(INPUT) as f:
+        chiton_map = []
+        for row in f.read().split('\n'):
+            chiton_map.append([int(c) for c in row])
+
+    low_risk = lowest_risk_path(chiton_map)
+    print(f'Ans 1 : {low_risk}')
     
 
 
