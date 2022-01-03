f640900ee637ce54868f925ae512b1a8153f187b
knotable <sjarnold1@gmail.com>
2022-01-03 10:59:49 +0000

    Operations function. No part 2 solution.

--- Day16/day16recursive.py
+++ Day16/day16recursive.py
@@ -1,4 +1,6 @@
 
+from numpy import prod
+
 HEX_TO_BITS = {
     '0' : '0000',
     '1' : '0001',
@@ -30,9 +32,9 @@ def hex_to_binary(s):
 def find_version(binary, v, sub_counter, sub=False):
     """Takes in string, position and values counter.
     Only uses sub_counter if sub = True.
+    Returns sum of version values.
     """
-    # if the binary list is empty (ie. we are at end), return version total
-    
+
     if (sub and sub_counter == 0) or (all([l == '0' for l in binary]) or not binary):
         return v
     
@@ -79,12 +81,180 @@ def find_version(binary, v, sub_counter, sub=False):
 
 
 
+
+def operation(id, values):
+
+    assert id in range(8)
+
+    if id == 0: return sum(values)
+
+    if id == 1: return prod(values)
+
+    if id == 2: return min(values)
+
+    if id == 3: return max(values)
+    
+    if id == 4: return values[0]
+    
+    if id == 5: return 1 if values[0] > values[1] else 0
+
+    if id == 6: return 1 if values[0] < values[1] else 0
+
+    if id == 7: return 1 if values[0] == values[1] else 0
+
+
+
+def find_values(binary, current_id, current_values, sub_counter, sub=False):
+    """Takes in string, position and values counter.
+    Only uses sub_counter if sub = True.
+    Returns sum of version values.
+    """
+
+    if (sub and sub_counter == 0) or (all([l == '0' for l in binary]) or not binary):
+        return operation(current_id, current_values)
+    
+    else:
+        # identify version and id (convert from binary)
+        current_id, binary = ''.join(binary[3:6]), binary[6:]
+        current_id = int(current_id,2)
+
+        if current_id != 4:
+            current_values = []
+            length_id = binary.pop(0)
+            if length_id == '0':
+                # If the length type ID is 0, then the next 15 bits are a number 
+                # that represents the total length in bits of the sub-packets contained by this packet.
+                length_of_subpacket, binary = ''.join(binary[0:15]), binary[15:]
+                length_of_subpacket = int(length_of_subpacket, 2)
+                subpacket, rest_of_string = binary[0:length_of_subpacket], binary[length_of_subpacket:]
+                sub_version_values = find_values(subpacket, current_id, current_values, 0)
+                return find_values(rest_of_string, current_id, sub_version_values, 0)
+
+            elif length_id == '1':
+                number_of_subpackets, binary = ''.join(binary[0:11]), binary[11:]
+                number_of_subpackets = int(number_of_subpackets, 2)
+                return find_values(binary, current_id, current_values, True)
+
+        if id == 4:
+            literal = '' 
+            # take off chunks of 5
+            while binary[0] == '1':
+                group, binary = binary[0:5], binary[5:]
+                literal += ''.join(group[1:])
+
+            group, binary = binary[0:5], binary[5:]
+            literal += ''.join(group[1:])
+
+            packet_value = int(literal, 2)
+            # print(f'packet_value: {packet_value}')
+
+            if sub:
+                sub_counter -= 1
+            return find_values(binary, current_id, [packet_value], sub_counter)
+
+
+
+
         # else perform operators 
+if __name__ == "__main__":
+
+    INPUT = '38006F45291200'
+    binary = hex_to_binary(INPUT)
+    v = find_version(binary, 0, 99999)
+    print(f'Answer 1: {v}')
 
 
-INPUT = 'A20D5080210CE4BB9BAFB001BD14A4574C014C004AE46A9B2E27297EECF0C013F00564776D7E3A825CAB8CD47B6C537DB99CD746674C1000D29BBC5AC80442966FB004C401F8771B61D8803D0B22E4682010EE7E59ACE5BC086003E3270AE4024E15C8010073B2FAD98E004333F9957BCB602E7024C01197AD452C01295CE2DC9934928B005DD258A6637F534CB3D89A944230043801A596B234B7E58509E88798029600BCF5B3BA114F5B3BA10C9E77BAF20FA4016FCDD13340118B929DD4FD54E60327C00BEB7002080AA850031400D002369400B10034400F30021400F20157D804AD400FE00034E000A6D001EB2004E5C00B9AE3AC3C300470029091ACADBFA048D656DFD126792187008635CD736B3231A51BA5EBDF42D4D299804F26B33C872E213C840022EC9C21FFB34EDE7C559C8964B43F8AD77570200FC66697AFEB6C757AC0179AB641E6AD9022006065CEA714A4D24C0179F8E795D3078026200FC118EB1B40010A8D11EA27100990200C45A83F12C401A8611D60A0803B1723542889537EFB24D6E0844004248B1980292D608D00423F49F9908049798B4452C0131006230C14868200FC668B50650043196A7F95569CF6B663341535DCFE919C464400A96DCE1C6B96D5EEFE60096006A400087C1E8610A4401887D1863AC99F9802DC00D34B5BCD72D6F36CB6E7D95EBC600013A88010A8271B6281803B12E124633006A2AC3A8AC600BCD07C9851008712DEAE83A802929DC51EE5EF5AE61BCD0648028596129C3B98129E5A9A329ADD62CCE0164DDF2F9343135CCE2137094A620E53FACF37299F0007392A0B2A7F0BA5F61B3349F3DFAEDE8C01797BD3F8BC48740140004322246A8A2200CC678651AA46F09AEB80191940029A9A9546E79764F7C9D608EA0174B63F815922999A84CE7F95C954D7FD9E0890047D2DC13B0042488259F4C0159922B0046565833828A00ACCD63D189D4983E800AFC955F211C700'
-binary = hex_to_binary(INPUT)
-v = find_version(binary, 0, 99999)
-print(f'Answer 1: {v})
+    def find_id(b):
+        """Takes current bitlist, pops off value and ID and length ID if value not equal
+        to 4. 
+        Returns id, length_id, remaining list."""
+        id, remainder = int(''.join(b[3:6]),2), b[6:]
+        length_id = remainder.pop(0) if id != 4 else -1
+        return id, length_id, remainder
+
+
+    def op_four(b):
+        """Takes current bitlist, assuming id was 4.
+        Determines the literal value, and the number of bits it took to get there.
+        Returns literal value, bits, remaining list."""
+        literal, bit_count = '', 0 
+        # take off chunks of 5
+        while b[0] == '1':
+            group, b = b[0:5], b[5:]
+            literal += ''.join(group[1:])
+            bit_count += 5
+
+        # last chunk
+        group, b = b[0:5], b[5:]
+        literal += ''.join(group[1:])
+        bit_count += 5
+
+        return int(literal, 2), bit_count, b
+
+    
+    def operation(id, values):
+        """Perform operation of ID on values (which should be a list)."""
+
+        assert id in range(8)
+        assert type(values) == list
+
+        if id == 0: return sum(values)
+
+        if id == 1: return prod(values)
+
+        if id == 2: return min(values)
+
+        if id == 3: return max(values)
+        
+        if id == 4: return values[0]
+        
+        if id == 5: return 1 if values[0] > values[1] else 0
+
+        if id == 6: return 1 if values[0] < values[1] else 0
+
+        if id == 7: return 1 if values[0] == values[1] else 0
+
+
+    def find_values(bits, current_packet):
+
+    # [id, [v1, v2]]
+    # [id1, [[id2, [v1, v2]], v3]]
+    # perform operation
+
+    # STRUCTURE:
+
+    # if we have reached the end of a section or sub-section, perform the operation on the values
+    # otherwise:
+        # find ID and value of next section
+        # perform literal operation if 4
+    
+    # end condition - only 0s or no list  
+    if all([l == '0' for l in bits]) or not bits: 
+        id, values = current_packet[0], current_packet[1]
+        return operation(id, values)
+
+    id, length_id, bits = find_id(bits)
+    current_packet = [id]
+    
+    if id == 4:
+        value, bit_count, bits = op_four(bits)
+        current_packet.append([value])
+        
+    else:
+        assert id in range(8) and id != 4
+        if bit_count == '0':
+            pass
+        else:
+            assert bit_count == '1'
+            pass
+
+
+
+
+    
+
+
+
+
 
 
