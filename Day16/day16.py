5e77911798eb8628066560b0339e2ea28bc9edc5
knotable <sjarnold1@gmail.com>
2022-01-03 10:59:28 +0000

    Original, unrecursive attempt

--- Day16/day16.py
+++ Day16/day16.py
@@ -27,8 +27,21 @@ def hex_to_binary(s):
     return binary
 
 
-def literal_number(s, p):
-    """Takes in string and pointer
+def identify_packet(s, p, v):
+    """Takes in string, position and values counter.
+    Returns version, id, updated pointer and updated values counter."""
+    version = int(s[p:p+3],2)
+    v += version
+    p += 3
+    id = int(s[p:p+3],2)
+    p += 3
+    return version, id, p, v
+
+
+def literal_number_packet(s, p):
+    """Takes in string and pointer, assuming id of packet is identified
+    as 4.
+    Operator 4 is the literal operator.
     Returns integer and updated pointer"""
     while int(s[p]) == 0:
         p += 1
@@ -45,6 +58,25 @@ def literal_number(s, p):
     return int(literal,2), p
 
 
+def operator_packet(s, p):
+    """Takes in string and pointer, assuming id of packet is not
+    identified as 4.
+    Returns length_id, number or length of subpackets, updated pointer."""
+    
+    length_id = s[p]
+    p += 1
+    
+    if length_id == '0':
+        n = int(s[p:p+15],2)
+        p += 15
+
+    if length_id == '1':
+        n = int(s[p:p+11],2)
+        p += 11
+    
+    return length_id, n, p
+
+
 eg = '38006F45291200'
 binary = hex_to_binary(eg)
 
@@ -54,27 +86,14 @@ parent = -99 # -99 means parent is master
 p = 0
 
 while p < len(binary):
-    version = int(binary[p:p+3],2)
-    version_numbers += version
-    p += 3
-    id = int(binary[p:p+3],2)
-    p += 3
+    version, id, p, v = identify_packet(binary, p, v)
 
     if id == 4:
-        n, p = literal_number(binary, p)
+        n, p = literal_number_packet(binary, p)
         packet_hierarchy.append([parent, version, id, n])
 
     else:
-        length_type_id = binary[p]
-        p += 1
-        if length_type_id == '0':
-            length_of_subpackets = int(binary[p:p+15],2)
-            p += 15
-            # keep going from here
+        length_id, n, p = operator_packet(binary, p)
+        
 
-
-
-        if length_type_id == '1':
-            number_of_subpackets = int(binary[p:p+11],2)
-            p += 11
 
