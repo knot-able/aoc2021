f15e34ecaf871b50ebfa4d32981adf27c1236da9
knotable <sjarnold1@gmail.com>
2022-01-08 13:46:07 +0000

    Day18 rescursive explode

--- Day18/day18.py
+++ Day18/day18.py
@@ -34,22 +34,47 @@ def list_depth(l):
         return 0
 
 
-def explode(num):
-    """Finds first pair nested inside four pairs, and EXPLODES it.
+def explode(snail_num, depth):
+    """Finds first pair nested inside four or more pairs, and EXPLODES it.
     Otherwise returns None.
     """
-    for i in range(len(num)):
-        if list_depth(num[i:]) == 4:
-            left, right = num[i][0], num[i][1]
-            first_left, first_right = i - 1, i + 1
-            # add to first 'regular' number to right and first regular number to left
-            if first_left >= 0:
-                num[first_left][1] += left
-            if first_right <= len(num):
-                num[first_right][0] += right
-            return num
+
+    # if list_depth(num) < 4:
+    #     return None
+
+    # # for i in range(len(num)):
+    # #     if list_depth(num[i:]) >= 4:
+    # #         left, right = num[i][0], num[i][1]
+    # #         first_left, first_right = i - 1, i + 1
+    # #         # add to first 'regular' number to right and first regular number to left
+    # #         if first_left >= 0:
+    # #             num[first_left][1] += left
+    # #         if first_right <= len(num):
+    # #             num[first_right][0] += right
+    # #         return num
+
+    # while True:
+
+    if all(isinstance(l,int) for l in snail_num):
+        if depth >= 4:
+            print(f'EXPLODE {snail_num}')
+            # TO DO, INSERT EXPLOSION
+        else:
+            return 0
+    
+    else:
+        depth += 1
+        left, right = snail_num[0], snail_num[1]
+        if isinstance(left, list):
+            return explode(left, depth)
+        else:
+            return explode(right, depth)    
+
+
             
-    return
+    # return
+
+
 
 
 
@@ -60,7 +85,9 @@ def explode(num):
 
 if __name__ == "__main__":
 
-    pass
-    # INPUT = "Day18/input18_test.txt"
-    # parsed_input = parse_input(INPUT)
-    # print(parsed_input[0])
+    INPUT = "Day18/input18_test.txt"
+    parsed_input = parse_input(INPUT)
+    print(explode([[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]],0))
+
+    # how to test for list presence
+    # print(all(isinstance(l,int) for l in [0,1,[0,1]]))
